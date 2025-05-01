68ae581a49f1c483b610b4c85da2e20d8c675d6e
E. Hannah <echannah631@gmail.com>
2025-05-01 12:17:15 -0400

    creating core values component & data, adding core value photos

---
+++ app/our-values/_data/coreValues.tsx
@@ -0,0 +1,34 @@
+import { CoreValueItem } from "@/utils/types";
+
+export const coreValues: CoreValueItem[] = [
+  {
+    title: "connection & community",
+    imageSrc: "/core-values/connection_community.png",
+    text: "Fostering deep, meaningful relationships and a sense of belonging.",
+  },
+  {
+    title: "authenticity",
+    imageSrc: "/core-values/authenticity.png",
+    text: "Promoting true self-expression and geniune connections.",
+  },
+  {
+    title: "exploration & growth",
+    imageSrc: "/core-values/exploration_growth.png",
+    text: "Encouraging curiosity, self-discovery, and continuous evolution.",
+  },
+  {
+    title: "creativity",
+    imageSrc: "/core-values/creativity.png",
+    text: "Inspiring new ideas, artistic expression, and innovative approaches to self-care.",
+  },
+  {
+    title: "intentionality",
+    imageSrc: "/core-values/intentionality.png",
+    text: "Crafting experiences with depth, purpose, and meaning.",
+  },
+  {
+    title: "joyful engagement",
+    imageSrc: "/core-values/joyful-engagement.png",
+    text: "Making self-care, wellness, and personal growth engaging, fun, and transformative.",
+  },
+];
