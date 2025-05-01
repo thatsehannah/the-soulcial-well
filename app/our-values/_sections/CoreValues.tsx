9c4478993a83f792e5875f8dc82ca6aab6bbeab5
E. Hannah <echannah631@gmail.com>
2025-05-01 00:10:18 -0400

    starting work on values page

---
+++ app/our-values/_sections/CoreValues.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CoreValues = () => {
+  return (
+    <section className='p-12'>
+      <p className='text-4xl lg:text-6xl text-center'>
+        Our Core <span className='font-script'>Values</span>
+      </p>
+    </section>
+  );
+};
+
+export default CoreValues;
