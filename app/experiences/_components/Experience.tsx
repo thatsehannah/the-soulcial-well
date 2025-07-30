267e4918c67616ef1ddaa5d6126eca1f4afd55ed
E. Hannah <echannah631@gmail.com>
2025-07-29 23:05:04 -0400

    making more cosmetic changes to experience page per client requests

--- app/experiences/_components/Experience.tsx
+++ app/experiences/_components/Experience.tsx
@@ -12,21 +12,20 @@ const Experience = ({ title, description, images, index }: ExperienceProps) => {
   const isEvenSection = index % 2 === 0;
 
   const formatTitle = (rawTitle: string) => {
+    const baseClass = "lg:text-6xl text-5xl text-white text-center";
+    const spanClass = `font-script ${
+      isEvenSection ? "text-main-foreground" : "text-primary"
+    } `;
+
     //for titles with a comma
     if (rawTitle.indexOf(",") !== -1) {
       const splitTitleWithComma = rawTitle.split(",");
 
       return (
-        <p className='lg:text-6xl text-5xl text-white text-center'>
+        <p className={baseClass}>
           {splitTitleWithComma[0]}
           {", "}
-          <span
-            className={`font-script ${
-              isEvenSection ? "text-main-foreground" : "text-primary"
-            } `}
-          >
-            {splitTitleWithComma[1]}
-          </span>
+          <span className={spanClass}>{splitTitleWithComma[1]}</span>
         </p>
       );
     }
@@ -35,29 +34,17 @@ const Experience = ({ title, description, images, index }: ExperienceProps) => {
 
     if (splitTitle[1] === "&" || splitTitle[1] === "+") {
       return (
-        <p className='lg:text-6xl text-5xl text-white text-center'>
+        <p className={baseClass}>
           {splitTitle[0]} {splitTitle[1]}{" "}
-          <span
-            className={`font-script ${
-              isEvenSection ? "text-main-foreground" : "text-primary"
-            } `}
-          >
-            {splitTitle.slice(2)}
-          </span>
+          <span className={spanClass}>{splitTitle.slice(2)}</span>
         </p>
       );
     }
 
     return (
-      <p className='lg:text-6xl text-5xl text-white text-center'>
+      <p className={baseClass}>
         {splitTitle[0]}{" "}
-        <span
-          className={`font-script ${
-            isEvenSection ? "text-main-foreground" : "text-primary"
-          } `}
-        >
-          {splitTitle.slice(1).join(" ")}
-        </span>
+        <span className={spanClass}>{splitTitle.slice(1).join(" ")}</span>
       </p>
     );
   };
@@ -69,9 +56,9 @@ const Experience = ({ title, description, images, index }: ExperienceProps) => {
       } px-12 py-24`}
     >
       <div className='mb-8'>{formatTitle(title)}</div>
-      <div className='flex lg:flex-row flex-col lg:my-8 my-4 w-full lg:gap-12 justify-center lg:items-start items-center'>
+      <div className='flex lg:flex-row flex-col lg:my-8 my-4 w-full lg:gap-12 justify-center items-center'>
         <FilmRoll images={images} />
-        <div className='lg:text-2xl text-lg lg:w-1/2 w-full lg:px-12 p-6'>
+        <div className='lg:text-[28px] lg:leading-9 leading-7 text-lg lg:w-1/2 w-full lg:px-12 p-6'>
           <p
             className={`${
               isEvenSection ? "text-main-foreground" : "text-white"
