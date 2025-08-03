9939b475b9c0ebb97632a965e6c012f119c144ca
E. Hannah <echannah631@gmail.com>
2025-08-02 20:45:14 -0400

    making responsive adjustments to origins and overview sections on about page

--- app/about/_sections/SoulcialWellOverview.tsx
+++ app/about/_sections/SoulcialWellOverview.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const SoulcialWellOverview = () => {
   return (
-    <section className='lg:p-32 pt-30 pb-12 px-4'>
-      <div className='grid grid-cols-1 lg:grid-cols-3 lg:mt-8 mt-4 w-full lg:gap-12 lg:p-8 p-2'>
-        <div className='flex justify-center items-center col-span-1'>
-          <div className='lg:block hidden'>
+    <section className='xl:p-32 pt-30 pb-12 px-4'>
+      <div className='grid grid-cols-1 xl:grid-cols-3 xl:mt-8 mt-4 w-full lg:gap-12 xl:p-8 p-2'>
+        <div className='xl:flex hidden justify-center items-center xl:col-span-1'>
+          <div>
             <Image
               src='/assets/overview.svg'
               alt='cross with logo inside'
@@ -14,10 +14,20 @@ const SoulcialWellOverview = () => {
             />
           </div>
         </div>
-        <div className='grid gap-4 text-xl col-span-2 lg:px-30 p-6'>
-          <p className='text-5xl lg:text-6xl text-center mb-4'>
+        <div className='grid gap-4 text-xl xl:col-span-2 xl:px-30 p-6'>
+          <p className='text-5xl lg:text-6xl text-center mb-8'>
             At The <span className='font-script'>Soulcial Well</span>
           </p>
+          <div className='xl:hidden flex justify-center items-center mb-8'>
+            <div>
+              <Image
+                src='/assets/overview.svg'
+                alt='cross with logo inside'
+                height={500}
+                width={500}
+              />
+            </div>
+          </div>
           <p>
             We believe that self-care is more than solo rituals—it’s about
             meaningful connection, shared experiences, and holistic well-being.
--- app/about/_sections/SoulciallWellOrigins.tsx
+++ app/about/_sections/SoulciallWellOrigins.tsx
@@ -3,12 +3,21 @@ import React from "react";
 
 const SoulcialWellOrigins = () => {
   return (
-    <section className='lg:p-32 pt-30 pb-12 px-4'>
-      <div className='grid grid-cols-1 lg:grid-cols-3 lg:mt-8 mt-4 w-full lg:gap-12'>
-        <div className='grid gap-4 text-xl col-span-2 lg:px-30 p-6'>
-          <p className='text-5xl lg:text-6xl text-center mb-4'>
+    <section className='xl:p-32 pt-8 pb-12 px-4 bg-[#f2f4e6]'>
+      <div className='grid grid-cols-1 xl:grid-cols-3 xl:mt-8 mt-4 w-full lg:gap-12 xl:p-8 p-2'>
+        <div className='grid gap-4 text-xl col-span-2 xl:px-30 p-6'>
+          <p className='text-5xl lg:text-6xl text-center mb-8'>
             How We <span className='font-script'>Started</span>
           </p>
+          <div className='xl:hidden flex justify-center items-center mb-8'>
+            <Image
+              src='/about-landing.svg'
+              alt='cross with logo inside'
+              height={500}
+              width={500}
+              quality={100}
+            />
+          </div>
           <p>
             At The Soulcial Well, we believe that self-care is more than solo
             rituals—it’s about meaningful connection, shared experiences, and
@@ -36,13 +45,14 @@ const SoulcialWellOrigins = () => {
             ✨
           </p>
         </div>
-        <div className='flex justify-center items-center col-span-1'>
-          <div className='lg:block hidden'>
+        <div className='flex justify-center items-center xl:col-span-1'>
+          <div className='xl:flex hidden'>
             <Image
               src='/about-landing.svg'
               alt='cross with logo inside'
               height={500}
               width={500}
+              quality={100}
             />
           </div>
         </div>
