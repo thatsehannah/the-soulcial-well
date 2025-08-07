cb727ac9938e4c7d0f8761584995bb9c50091d59
E. Hannah <echannah631@gmail.com>
2025-08-07 10:00:56 -0600

    using react hook form and yup for form construction and validation, making sure submit buttton is working

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -11,10 +13,41 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { services } from "@/utils/services";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Image from "next/image";
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const contactSchema = yup.object({
+  name: yup.string().required("Please enter your name"),
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Please enter your email"),
+  service: yup.string().required("Please select a service"),
+  message: yup.string().required("Please enter your message"),
+});
 
 const Contact = () => {
+  const form = useForm({ resolver: yupResolver(contactSchema) });
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+    clearErrors,
+  } = form;
+  const selectedService = watch("service");
+
+  const onSubmit = (data: yup.InferType<typeof contactSchema>) => {
+    //TODO: hook up render email to send emails
+    console.log(data);
+    form.reset();
+  };
+
   return (
     <main className='xl:p-32 pt-38 pb-12 px-7'>
       <section className='grid lg:grid-cols-2 grid-cols-1 gap-2 w-full'>
@@ -28,49 +61,93 @@ const Contact = () => {
           />
         </div>
         <div className='flex flex-col justify-center items-center p-8'>
-          <p className='relative text-5xl lg:text-6xl text-center text-primary after:bg-[url("/assets/underline-stroke-brown.svg")] after:absolute after:left-0 lg:after:-bottom-13 after:-bottom-9 after:w-full after:h-13 after:bg-no-repeat after:bg-contain after:bg-center'>
+          <p className='relative text-5xl lg:text-6xl text-center text-primary after:bg-[url("/assets/underline-stroke-brown.svg")] after:absolute after:left-0 after:-bottom-8 after:w-full after:h-13 after:bg-no-repeat after:bg-contain after:bg-center'>
             Contact <span className='font-script'>Us</span>
           </p>
-          <div className='flex flex-col p-8 bg-main-foreground rounded-4xl h-fit lg:w-xl w-full text-lg mt-20'>
-            <Input
-              type='text'
-              placeholder='Name'
-              className='bg-main-bg placeholder:font-script placeholder:text-xl text-black'
-            />
-            <Input
-              type='email'
-              placeholder='Email'
-              className='bg-main-bg mt-3 placeholder:font-script placeholder:text-xl text-black'
-            />
-            <Select>
-              <SelectTrigger className='w-full mt-3 bg-main-bg data-[placeholder]:font-script data-[placeholder]:text-xl text-black'>
-                <SelectValue
-                  className='text-black'
-                  placeholder='Service interested in'
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex flex-col p-8 bg-main-foreground rounded-4xl h-fit lg:w-xl w-full text-lg mt-20'>
+              <div>
+                <Input
+                  type='text'
+                  placeholder='Name'
+                  className='bg-main-bg placeholder:font-script placeholder:text-xl text-black'
+                  {...register("name")}
                 />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel className='font-script text-xl'>
-                    Services
-                  </SelectLabel>
-                  {services.map((svc, idx) => (
-                    <SelectItem
-                      value={svc}
-                      key={idx}
-                    >
-                      {svc}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Textarea
-              className='bg-main-bg mt-15 placeholder:font-script placeholder:text-xl text-black'
-              placeholder='Tell us more...'
-            />
-            <Button className='mt-4 w-fit'>Submit</Button>
-          </div>
+                {errors.name && (
+                  <p className='font-bold text-destructive'>
+                    {errors.name.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <Input
+                  type='email'
+                  placeholder='Email'
+                  className='bg-main-bg mt-3 placeholder:font-script placeholder:text-xl text-black'
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className='font-bold text-destructive'>
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <Select
+                  value={selectedService ?? ""}
+                  onValueChange={(value) => {
+                    setValue("service", value);
+                    clearErrors("service");
+                  }}
+                >
+                  <SelectTrigger className='w-full mt-3 bg-main-bg data-[placeholder]:font-script data-[placeholder]:text-xl text-black'>
+                    <SelectValue
+                      className='text-black'
+                      placeholder='Service interested in'
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel className='font-script text-xl'>
+                        Services
+                      </SelectLabel>
+                      {services.map((svc, idx) => (
+                        <SelectItem
+                          value={svc}
+                          key={idx}
+                        >
+                          {svc}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                {errors.service && (
+                  <p className='font-bold text-destructive'>
+                    {errors.service.message}
+                  </p>
+                )}
+              </div>
+              <div>
+                <Textarea
+                  className='bg-main-bg mt-15 placeholder:font-script placeholder:text-xl text-black'
+                  placeholder='Tell us more...'
+                  {...register("message")}
+                />
+                {errors.message && (
+                  <p className='font-bold text-destructive'>
+                    {errors.message.message}
+                  </p>
+                )}
+              </div>
+              <Button
+                type='submit'
+                className='mt-4 w-fit'
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
         </div>
       </section>
     </main>
