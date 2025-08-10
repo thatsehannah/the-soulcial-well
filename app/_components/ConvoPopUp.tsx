4c65edd6d7b29b96abe71999179aa2945125a38d
E. Hannah <echannah631@gmail.com>
2025-08-09 18:28:55 -0600

    creating a dialog for daily convo question, setting timer to delay opening dialog after page loads

---
+++ app/_components/ConvoPopUp.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import React, { useEffect, useState } from "react";
+
+const ConvoPopUp = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsOpen(true);
+    }, 6500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={setIsOpen}
+    >
+      <DialogContent className='min-w-[80%] border-4 border-primary text-center'>
+        <DialogHeader>
+          <DialogTitle className='font-normal lg:text-3xl text-3xl'>
+            Your Daily{" "}
+            <span className='font-script font-bold'>Candid Conversation</span>
+          </DialogTitle>
+          <DialogDescription className='text-lg'>
+            Thank you for visiting. Take a moment to think about the following
+            reflection question - just for you!
+          </DialogDescription>
+        </DialogHeader>
+        <p className='lg:text-5xl text-3xl font-bold'>
+          What&apos;s a version of me do I secretly miss?
+        </p>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ConvoPopUp;
