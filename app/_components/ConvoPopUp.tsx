df11e6f1f7ca8d10f45dd798a25a827cf940696a
E. Hannah <echannah631@gmail.com>
2025-08-11 00:39:06 -0600

    working out logic to reset localstorage at midnight

--- app/_components/ConvoPopUp.tsx
+++ app/_components/ConvoPopUp.tsx
@@ -7,20 +7,39 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ConvoPopUp = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const dialogRef = useRef(null);
 
   useEffect(() => {
-    if (localStorage.getItem("visitedToday") === null) {
-      localStorage.setItem("visitedToday", "true");
+    const resetPopUpExpiry = () => {
+      const currentTime = Date.now();
+      const targetTime = new Date(currentTime).setHours(23, 59, 0, 0);
+      console.log("Target time: " + targetTime);
+
+      console.log("expiry calculation: " + (targetTime - currentTime));
+      const expiryTime = currentTime + (targetTime - currentTime).toString();
+      console.log("Getting ready to set expiryTime: " + expiryTime);
+
+      localStorage.setItem("popUpExpiry", expiryTime);
       const timeout = setTimeout(() => {
         setIsOpen(true);
       }, 6500);
 
       return () => clearTimeout(timeout);
+    };
+
+    if (localStorage.getItem("popUpExpiry") === null) {
+      resetPopUpExpiry();
+    }
+
+    const expiryTime = localStorage.getItem("popUpExpiry");
+    if (expiryTime && Date.now().toString() >= expiryTime) {
+      console.log(Date.now().toString() + " > " + expiryTime);
+      console.log("Removing expiryTime: " + expiryTime);
+      localStorage.removeItem("popUpExpiry");
+      resetPopUpExpiry();
     }
   }, []);
 
@@ -29,14 +48,12 @@ const ConvoPopUp = () => {
       open={isOpen}
       onOpenChange={setIsOpen}
     >
-      <DialogContent
-        ref={dialogRef}
-        className='min-w-[80%] border-4 border-primary text-center'
-      >
+      <DialogContent className='min-w-[80%] border-4 border-primary text-center'>
         <DialogHeader>
           <DialogTitle className='font-normal lg:text-3xl text-3xl'>
             Your Daily{" "}
-            <span className='font-script font-bold'>Candid Conversation</span>
+            <span className='font-script font-bold'>Candid Conversation</span>{" "}
+            ❤️
           </DialogTitle>
           <DialogDescription className='text-lg'>
             Thank you for visiting. Take a moment to think about the following
