3cb0607319010d6b9df5c30467e7b2922346717b
E. Hannah <echannah631@gmail.com>
2025-09-04 10:45:48 -0400

    making responsive adjustments

--- app/experiences/_components/UpcomingExperience.tsx
+++ app/experiences/_components/UpcomingExperience.tsx
@@ -39,15 +39,26 @@ const UpcomingExperience = ({ experience }: { experience: ExperienceItem }) => {
 
   return (
     <div className='flex lg:flex-row flex-col lg:my-8 my-4 w-full lg:gap-12 justify-center items-center'>
-      <div className='text-xl lg:leading-9 leading-7 lg:w-1/2 w-full lg:px-12 px-0 pt-4'>
-        <div className='lg:text-6xl text-5xl mb-12 text-primary title text-shadow-lg'>
+      <div className='text-xl lg:leading-9 leading-7 lg:w-1/2 w-full lg:px-12 px-0 pt-4 flex flex-col lg:items-start items-center justify-center'>
+        <div className='lg:text-6xl text-5xl lg:mb-12 mb-8 text-primary title text-shadow-lg'>
           {formatTitle(experience.title)}
+        </div>
+        <div className='lg:hidden block h-[410px] w-[290px] relative lg:mb-12 mb-8'>
+          <Image
+            src={flyerUrl!}
+            alt='flyer'
+            fill
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            quality={100}
+            className='rounded-xl shadow-2xl absolute image'
+            priority
+          />
         </div>
         <p className='text-white whitespace-pre-wrap up-description'>
           {upcomingDescription}
         </p>
         {linkToRsvp && (
-          <Button className='flex justify-center items-center gap-2 text-2xl text-black mt-12 p-6 hover:scale-110 hover:cursor-pointer hover:shadow-2xl transition-all ease-in-out duration-300 button'>
+          <Button className='flex justify-center items-center gap-2 text-2xl text-black lg:mt-12 mt-8 p-6 hover:scale-110 hover:cursor-pointer hover:shadow-2xl transition-all ease-in-out duration-300 button'>
             <a
               href={linkToRsvp!}
               target='_blank'
@@ -57,7 +68,7 @@ const UpcomingExperience = ({ experience }: { experience: ExperienceItem }) => {
           </Button>
         )}
       </div>
-      <div className='h-[550px] w-[450px] relative'>
+      <div className='lg:block hidden h-[550px] w-[450px] relative'>
         <Image
           src={flyerUrl!}
           alt='flyer'
@@ -98,23 +109,25 @@ const UpcomingExperiences = ({ experiences }: UpcomingExperienceProps) => {
           Upcoming Experiences
         </p>
       </div>
-      <div className='flex justify-between items-center'>
+      <div className='flex items-center justify-center relative'>
         {isPrevButtonVisible && (
-          <CircleArrowLeft
-            size={42}
-            color='white'
-            className={`hover:scale-110 hover:fill-primary hover:cursor-pointer transition-all ease-in-out duration-300 ${isPrevButtonVisible ? "visible" : "invisible"}`}
-            onClick={() => setCurrentIndex((prevState) => prevState - 1)}
-          />
+          <div className='absolute lg:block lg:left-0 -left-[12%]'>
+            <CircleArrowLeft
+              color='white'
+              className='hover:scale-110 hover:fill-primary hover:cursor-pointer transition-all ease-in-out duration-300 lg:w-10 w-8 lg:h-10 h-8'
+              onClick={() => setCurrentIndex((prevState) => prevState - 1)}
+            />
+          </div>
         )}
         <UpcomingExperience experience={currentUpcomingExperience} />
         {isNextButtonVisible && (
-          <CircleArrowRight
-            size={42}
-            color='white'
-            className='hover:scale-110 hover:fill-primary hover:cursor-pointer transition-all ease-in-out duration-300'
-            onClick={() => setCurrentIndex((prevState) => prevState + 1)}
-          />
+          <div className='absolute lg:block lg:right-0 -right-[12%]'>
+            <CircleArrowRight
+              color='white'
+              className='hover:scale-110 hover:fill-primary hover:cursor-pointer transition-all ease-in-out duration-300 lg:w-10 w-8 lg:h-10 h-8'
+              onClick={() => setCurrentIndex((prevState) => prevState + 1)}
+            />
+          </div>
         )}
       </div>
     </div>
