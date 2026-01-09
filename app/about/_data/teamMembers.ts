afc958248abe1ebb430d4b6caa4b9008117f3982
E. Hannah <echannah631@gmail.com>
2026-01-09 10:17:35 -0500

    hotfix: removing team members

--- app/about/_data/teamMembers.ts
+++ app/about/_data/teamMembers.ts
@@ -26,68 +26,6 @@ A free spirit with a love for bold ideas, offbeat conversations, and themed expe
 ✨ Ready to rethink self-care? Connect with Dr. Wilma and step into a world where wellness is anything but routine.`,
   },
   {
-    imageSrc: "/staff-images/shantay.png",
-    firstName: "Shantay",
-    lastName: "Saulsberry",
-    title: "Connection Curator",
-    connections: {
-      email: "",
-    },
-    aboutMeSubtitle: "Good vibes, great convos, and building community.",
-    recentEducation: {
-      year: "2023",
-      degree: "Doctorate of Clinical Forensic Psychology",
-      school: "The Chicago School",
-    },
-    bio: `Shantay is a enthusiastic connection curator whether its through civic engagement or a night out with friends. With a background in social work and community advocacy, she is passionate about helping people feel empowered and connected.
-
-When she is not rallying folks to give public comment or speaking at conferences, you can find her testing new recipes, at Zumba, or planning the next fun hangout. Shantay believe in showing up for what matters most and making space for joy, laughter, and meaningful connections.✨
-
-✨ Ready to community? Connect with Shantay and step into a world where wellness is anything but routine.
-`,
-  },
-  {
-    imageSrc: "/staff-images/jassmyn.png",
-    firstName: "Jassmyn",
-    lastName: "Stevens",
-    title: "Operations Alchemist",
-    connections: {
-      email: "",
-    },
-    aboutMeSubtitle:
-      "Meaningful connections, greater sense of self, community engagement.",
-    recentEducation: {
-      year: "2023",
-      degree: "Doctorate of Clinical Forensic Psychology",
-      school: "The Chicago School",
-    },
-    bio: `Jassmyn classifies herself as someone who has a deep curiosity about the world. She is passionate about exploring diverse perspectives and uncovering what connects us. Jassmyn strives to build meaningful relationships and promote wellbeing in both personal and professional spaces.
-
-✨ Ready to re-think self-care? Connect with Jassmyn and step into a world where wellness is anything but routine.
-`,
-  },
-  {
-    imageSrc: "/staff-images/tisnue.png",
-    firstName: "Tisnue",
-    lastName: "Jean-Baptiste",
-    title: "Insight & Design Strategist",
-    connections: {
-      email: "",
-    },
-    aboutMeSubtitle: "Compassion, curiosity, and creativity.",
-    recentEducation: {
-      year: "2023",
-      degree: "Doctorate of Clinical Forensic Psychology",
-      school: "The Chicago School",
-    },
-    bio: `Tisnue Jean-Baptiste is a doctoral student at Rice University in Psychology. She studes how stigmatized employees overcome overwhelming experiences. Her name means "strength" in Amharic which is an attribute she finds personally and professionally meaningful.
-
-As a psychologist she finds it important to identify personal strengths to broaden thinking and build resources. Thus as a human being, one our most valuable resources is our network of social support which is what lead Tisnue to The Soulcial Well. She enjoys crocheting, listening to audiobooks, and going to live music events in Houston.
-
-✨ Ready to re-think self-care? Connect with Tisnue and step into a world where wellness is anything but routine.
-`,
-  },
-  {
     imageSrc: "/staff-images/bianca.png",
     firstName: "Bianca",
     lastName: "Wentt",
